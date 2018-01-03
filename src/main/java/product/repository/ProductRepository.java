6a7fe251e8d374fb01749404aed918f878d9e867
Gina <paalginaa@gmail.com>
2018-01-03 10:21:17 +0100

    Product class initialization

---
+++ src/main/java/product/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package product.repository;
+
+import product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
